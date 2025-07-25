6765a80cb409a9d4ad563a53450c6e15817c3a8a
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-25 13:26:09 +0200

    Add Carousel component with navigation and auto-slide functionality

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,81 @@
+import React, { useReducer, useEffect } from "react";
+import { list } from "../data";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+const NEXT = "NEXT";
+const PREV = "PREV";
+
+const defaultState = {
+  people: list,
+  currentPerson: 0,
+};
+
+const reducer = (state, action) => {
+  if (action.type === NEXT) {
+    const newPerson = state.currentPerson + 1;
+    return {
+      ...state,
+      currentPerson: newPerson >= state.people.length ? 0 : newPerson,
+    };
+  }
+  if (action.type === PREV) {
+    const newPerson = state.currentPerson - 1;
+    return {
+      ...state,
+      currentPerson: newPerson < 0 ? state.people.length - 1 : newPerson,
+    };
+  }
+};
+
+const Carousel = () => {
+  const [state, dispatch] = useReducer(reducer, defaultState);
+
+  useEffect(() => {
+    const slider = setTimeout(() => {
+      dispatch({ type: NEXT });
+    }, 4000);
+    return () => clearInterval(slider);
+  }, [state.currentPerson]);
+
+  return (
+    <section className="slider-container">
+      {state.people.map(({ id, image, name, title, quote }, personIndex) => {
+        return (
+          <article
+            className="slide"
+            style={{
+              transform: `translateX(${
+                100 * (personIndex - state.currentPerson)
+              }%)`,
+            }}
+            key={id}
+          >
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+
+      <button
+        type="button"
+        className="prev"
+        onClick={() => dispatch({ type: PREV })}
+      >
+        <FiChevronLeft />
+      </button>
+      <button
+        type="button"
+        className="next"
+        onClick={() => dispatch({ type: NEXT })}
+      >
+        <FiChevronRight />
+      </button>
+    </section>
+  );
+};
+
+export default Carousel;
