01d0339b8e92470afc9be49e1dc3beca848158ba
tsheporamantso <tshepo.ramantso@outlook.com>
2025-07-25 19:06:39 +0200

    Refactored code and add silk react carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Carousel from "./components/Carousel";
+import SlickCarousel from "./components/SlickCarousel";
 
 function App() {
   return (
-    <>
-      <Carousel />
-    </>
+    <main>
+      {/* <Carousel /> */}
+      <SlickCarousel />
+    </main>
   );
 }
 
